e4e76e435d2445439d2c918379d9a88e6b8aca24
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-12-08 20:06:03 +0700

    Update open_pdf

--- source/modules/library/Open_pdf.py
+++ source/modules/library/Open_pdf.py
@@ -8,19 +8,19 @@ def open(compression_pressure, Pmax, Pmin, Minimum_pressure_intake ):
         path = "F:\HK211\Luan_van\source\library\libary_fix\Open.txt"
     else:
         value = 'Hư hỏng'
-        path_open = "F:\HK211\Luận Văn\source\library\libary_fix\Tháo lắp động cơ.pdf"
+        path_open = "F:\HK211\Luan_van\source\library\libary_fix\Tháo lắp động cơ.pdf"
         if Pmax < compression_pressure:
-            path = 'F:\HK211\Luận Văn\source\library\libary_fix\Gãy xéc măng, gãy xupap lủng piston.pdf'
+            path = 'F:\HK211\Luan_van\source\library\libary_fix\Gãy xéc măng, gãy xupap lủng piston.pdf'
         elif compression_pressure < Pmax <0.62*compression_pressure:
-            path = 'F:\HK211\Luận Văn\source\library\libary_fix\Hở giăng nắp máy.pdf'
+            path = 'F:\HK211\Luan_van\source\library\libary_fix\Hở giăng nắp máy.pdf'
         elif 0.62*compression_pressure < Pmax <0.8*compression_pressure:
-            path = 'F:\HK211\Luận Văn\source\library\libary_fix\Hở xupap.pdf'
+            path = 'F:\HK211\Luan_van\source\library\libary_fix\Hở xupap.pdf'
         elif Pmax > compression_pressure:
-            path = 'F:\HK211\Luận Văn\source\library\libary_fix\Xilanh không đều.pdf'
+            path = 'F:\HK211\Luan_van\source\library\libary_fix\Xilanh không đều.pdf'
         elif Pmax > 0.9*compression_pressure:
-            path = 'F:\HK211\Luận Văn\source\library\libary_fix\Xilanh không đều.pdf'
+            path = 'F:\HK211\Luan_van\source\library\libary_fix\Xilanh không đều.pdf'
         elif Pmin < 0.8*Minimum_pressure_intake:
-            path = 'F:\HK211\Luận Văn\source\library\libary_fix\Hở xupap.pdf'
+            path = 'F:\HK211\Luan_van\source\library\libary_fix\Hở xupap.pdf'
         elif Pmin > 0.62*Minimum_pressure_intake:
-            path = 'F:\HK211\Luận Văn\source\library\libary_fix\Hở giăng nắp máy.pdf'
+            path = 'F:\HK211\Luan_van\source\library\libary_fix\Hở giăng nắp máy.pdf'
     return  path_open,path
